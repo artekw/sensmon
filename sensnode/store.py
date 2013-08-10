4539632586cd6687a13bb87132afea77d2245c31
arteq <arteq@gmail.com>
2013-08-10 19:59:19 +0200

    dodano generowanie kluczy, tworzenie punow w timestore

--- sensnode/store.py
+++ sensnode/store.py
@@ -55,15 +55,44 @@ class History():
     Baza Timestore
     http://www.mike-stirling.com/redmine/projects/timestore
     """
-    def __init__(self, host='127.0.0.1'):
-        tsdb = Client(host)
+    def __init__(self, host='127.0.0.1:8080'):
+        self.tsdb = Client(host)
+        self.adminkey = self.getAdminKey()
 
-    def createNode():
+    def createNode(self, node_id, interval, public=False):
         """Utwórz node"""
-        pass
+
+        array = {
+                'interval': interval,
+                'decimation': [20, 6, 6, 4, 7],
+                'metrics': [ {
+                            'pad_mode' : 0,
+                            'downsample_mode' : 0
+                            } ]
+                }
+
+        try:
+            self.tsdb.create_node(node_id, array, key = self.adminkey)
+            node_created = True
+        except TimestoreException as e:
+            node_created = False
+            if e.status == 403:
+                print "Node exist?"
+                try:
+                    print "Try to erase node..."
+                    self.tsdb.delete_node(node_id, key = self.adminkey)
+                    print "Deleted, try to new one"
+                except TimestoreException as e:
+                    if e.status == 403:
+                        print "Admin key rejected!"
+                        raise
+            else:
+                raise
+        if node_created and not public:
+            self.setRWKeys(node_id)
 
     def getAdminKey(self, path='/var/lib/timestore'):
-        """Ustaw klucz admina"""
+        """Pobierz klucz admina"""
 
         if os.path.isfile('%s/%s' % (path, 'adminkey.txt')):
             with open('%s/%s' % (path, 'adminkey.txt'), 'r') as adminkey_file:
@@ -73,9 +102,18 @@ class History():
             logging.warning('Uruchom bazę timestore, brak pliku z kluczem admina!')
             pass
 
-    def setRWKeys(self, node, tskey):
+    def setRWKeys(self, node_id):
         "Wygeneruj klucz do zapisu i odczytu"
-        rand = random.random()
 
-        ts.set_key()
-        pass
+        m = hashlib.md5()
+        m.update(str(random.random()))
+        readkey = m.hexdigest()
+
+        m.update(str(random.random()))
+        writekey = m.hexdigest()
+
+        self.tsdb.set_key(node_id, 'read', readkey, key = self.adminkey)
+        self.tsdb.set_key(node_id, 'write', writekey, key = self.adminkey)
+        print 'Read key: %s' % (readkey)
+        print 'Write key: %s' % (writekey)
+        print 'Keys generated!'
