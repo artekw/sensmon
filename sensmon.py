3a0e7a7ea6176f14ba6391fc64090a140114eeca
arteq <arteq@gmail.com>
2013-08-15 12:05:27 +0200

    ma wyswietlac ciag posortowany

--- sensmon.py
+++ sensmon.py
@@ -284,7 +284,7 @@ def main():
             # filtr - FIXME
             decodedj = json.loads(decoded)
             if debug:
-                print "JSON: %s" % (decodedj)
+                print "JSON: %s" % (decoded)
             if decodedj['name'] not in filterout:
                 yield cdb.save({'msg': decoded})
             # koniec
