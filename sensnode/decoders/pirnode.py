e29ccc491f93f775fade73fa34802bb91f369fca
arteq <arteq@gmail.com>
2013-08-17 13:07:38 +0200

    poprawki w pirnode

--- sensnode/decoders/pirnode.py
+++ sensnode/decoders/pirnode.py
@@ -8,19 +8,13 @@ def pirnode(data, name):
     """Czujka ruchu"""
 
     a = int(data[2])
-    b = int(data[3])
-    c = int(data[4])
-    d = int(data[5])
 
     #nodeid = str(data[1])
     timestamp  = int(time.time())
 
     template = ({
         'name':name,
-        'light': a,
         'motion': b & 1,
-        'humi': b >> 1,
-        'temp': str(((256 * (d&3) + c) ^ 512) - 512),
         'lowbat': (d >> 2) & 1,
         'timestamp':timestamp
     })
