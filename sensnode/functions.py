256b7574f8ff8218fb81b1c84a56180957857219
arteq <arteq@gmail.com>
2013-09-02 21:54:43 +0200

    wstepna implementacja

--- sensnode/functions.py
+++ sensnode/functions.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-class alarms():
-    def __init__(self):
+# import smtplib
+
+import common
+import logging
+import config
+
+class alarms(object):
+    def __init__(self, debug=False):
+        self.settings_cfg = common.settings_cfg
+        self.nodes_cfg = common.nodes_cfg
+        self.debug = debug
+        pass
+
+    def onAlarm():
+        pass
+
+    def sendMail():
         pass
