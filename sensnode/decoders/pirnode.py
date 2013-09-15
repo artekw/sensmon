6f3a76ee65ca1bb1aa6b56e23d2951ecbd329965
arteq <arteq@gmail.com>
2013-09-15 19:51:52 +0200

    przeniesienie kodu: czujka ruchu

--- sensnode/decoders/pirnode.py
+++ sensnode/decoders/pirnode.py
@@ -8,15 +8,19 @@ def pirnode(data, name):
     """Czujka ruchu"""
 
     a = int(data[2])
+    b = int(data[3])
+    c = int(data[4])
+    d = int(data[5])
 
     #nodeid = str(data[1])
     timestamp  = int(time.time())
 
     template = ({
-        'name':name,
-        'motion': a & 1,
-        'lowbat': (a >> 2) & 1,
-        'timestamp':timestamp
+        'light': a,
+        'motion': b & 1,
+        'humi': b >> 1,
+        'temp': str(((256 * (d&3) + c) ^ 512) - 512),
+        'lowbat': (d >> 2) & 1,
     })
 
     return dict((k,v) for (k,v) in template.iteritems())
